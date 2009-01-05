30f353b3546f15413cde80163ac5c0e603d8f9f9
percious <none@none>
2009-01-05 00:05:20 -0500

    tagging 0.5rc3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="sprox",
-  version="0.5rc2",
+  version="0.5rc3",
   zip_safe=False,
   include_package_data=True,
   description="""A package for creation of web widgets directly from database schema.""",
