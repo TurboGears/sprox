e7d7f9954a5aed6b54ab9b9c3d084e72817b168c
percious <none@none>
2009-08-28 13:34:50 -0600

    changing stuff back the way it was.

--- sprox/widgets/static/dojo_grid.js
+++ sprox/widgets/static/dojo_grid.js
@@ -3,9 +3,3 @@ dojo.require("dojo.parser");
 dojo.require("dojox.data.QueryReadStore");
 dojo.require("dojox.grid.DataGrid");
 dojo.require("dojo._base.xhr");
-dojo.__XhrArgs = {preventCache:true}
-//dojo.addOnLoad(
-//    function(){
-//        dojo.parser.parse();
-//    });
-
