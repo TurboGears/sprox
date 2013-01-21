63531ccf6ad786f83bda5861de4fff355028e444
Alessandro Molina <alessandro.molina@axant.it>
2013-01-21 19:47:56 +0100

    Skip tw2 widgets tests on tw1

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,5 +1,6 @@
 from sprox.widgets import SproxCheckBox, PropertyMultipleSelectField
 from nose.tools import raises, eq_
+from nose import SkipTest
 from formencode.validators import Int as IntValidator
 
 class TestSproxCheckbox:
@@ -15,7 +16,11 @@ class TestMultipleSelection:
                                                   validator=IntValidator())
 
     def test_multiple_selection_single_entry(self):
+        if not hasattr(self.widget, 'req'):
+            raise SkipTest('Test for Tw2')
         self.widget.req()._validate('1') == [1]
 
     def test_multiple_selection_invalid_entries(self):
+        if not hasattr(self.widget, 'req'):
+            raise SkipTest('Test for TW2')
         self.widget.req()._validate(['a', 'b']) == []
