cb8d6bf304bb01144bf59f6b114cdda4f22f24a9
Vincenzo Castiglia <vincenzo.castiglia@axant.it>
2021-04-29 11:59:42 +0200

    fix tests for py3.5, 3.6 and 3.7

--- tests/test_testbase.py
+++ tests/test_testbase.py
@@ -1,20 +1,30 @@
 from sprox.test.base import eq_xml, in_xml, fix_xml
+from sys import version_info
+
+# unordered attrs (python<3.8) are somewhat consistent anyway
+attrs_order_kept = version_info[0] >= 3 and version_info[1] >= 8
 
 def test_fix_xml():
     s = """<form action="" method="post" class="required tableform">
         <div></div>
         </form>"""
-    e = """<form action="" method="post" class="required tableform"><div /></form>"""
-    r =fix_xml(s)
-    assert r == e, (r, s)
+    if attrs_order_kept:
+        e = """<form action="" method="post" class="required tableform"><div /></form>"""
+    else:
+        e = """<form action="" class="required tableform" method="post"><div /></form>"""
+    r = fix_xml(s)
+    assert r == e, (r, e)
 
 def test_fix_xml_with_escapes():
     s = """<form action="" method="post" class="required tableform">
         <div></div>&nbsp;
         </form>"""
-    e = """<form action="" method="post" class="required tableform"><div /></form>"""
-    r =fix_xml(s)
-    assert r == e, r
+    if attrs_order_kept:
+        e = """<form action="" method="post" class="required tableform"><div /></form>"""
+    else:
+        e = """<form action="" class="required tableform" method="post"><div /></form>"""
+    r = fix_xml(s)
+    assert r == e, (r, e)
 
 def test_fix_xml_with_namespace():
     s = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
@@ -27,4 +37,4 @@ def test_fix_xml_with_namespace():
     </body>
     </html>"""
     e = """<html><body><form action="" class="required tableform" method="post"><div /></form></body></html>"""
-    r =fix_xml(s)
+    r = fix_xml(s)
